599d037f790c8b58fdf111ebf152720bf0b4e3ce
Martin Bäumer <martin.baeumer@gmail.com>
2023-07-30 22:03:06 +0200

    added dummy database service

---
+++ src/main/java/se/mbaeumer/solidexample/DbService.java
@@ -0,0 +1,24 @@
+package se.mbaeumer.solidexample;
+
+public class DbService {
+
+    public void saveOrder(){
+
+    }
+
+    public void deleteOrder(){
+
+    }
+
+    public void updateOrder(){
+
+    }
+
+    public void getAllOrders(){
+
+    }
+
+    public void getOrderById(){
+        
+    }
+}
